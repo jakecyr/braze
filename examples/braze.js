c931d5b5001859f4ad6cedb45bad94cbe7e94e47
Jake <jake@Jakes-MacBook-Air.local>
2020-02-09 17:12:06 -0500

    Update config type

---
+++ examples/braze.js
@@ -0,0 +1,9 @@
+module.exports = {
+    pagesDir: "./pages",
+    outputDir: "./dist",
+    componentsDir: "./components",
+    props: {
+        "people": ["John", "Sam", "Alex"],
+    },
+    minifyOutput: true,
+};
